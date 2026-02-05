336f1dec4b9558c8c26342b59312236dc57b6dcf
incon-science <1conscienceofficiel@gmail.com>
2026-02-05 16:02:22 +0100

    Add new elements and functionality to the game
    - Introduced a new texture import for 'basik_sobre.png' to enhance visual assets.
    - Created a new scene 'cadavre_exquis.tscn' with animated sprites using an atlas texture.
    - Added a new script 'temps.gd' to display a time-related label in the game.
    - Defined a new scene 'temps.tscn' to utilize the 'temps.gd' script for displaying time.
    - Implemented 'portal_room.gd' to manage player interactions and sprite layering based on player position.

---
+++ src/levels/portal_room.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var player: Player = $Player
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player.global_position.y < -178 :
+		sprite_2d.z_index = 2
+	else :
+		sprite_2d.z_index = 0
--- src/vivants/mouse.gd
+++ src/vivants/mouse.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 120.0
 const JUMP_VELOCITY = -400.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+var rng = RandomNumberGenerator.new()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,10 +15,20 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := 0
+	var direction = rng.randi_range(0,0)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if velocity.x != 0:
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+		
+	if velocity.x < 0 :
+		animated_sprite_2d.flip_h = true
+	else :
+		animated_sprite_2d.flip_h = false
 
 	move_and_slide()
