4e587f4efed5ddbf044b6d50ed22f30478221ce8
incon-science <1conscienceofficiel@gmail.com>
2026-02-10 20:15:06 +0100

    Update main scene reference and add start scene with UI elements

--- src/global.gd
+++ src/global.gd
@@ -9,5 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("start"):
-		get_tree().reload_current_scene()
+		#get_tree().reload_current_scene()
+		get_tree().change_scene_to_file("res://src/levels/room_blueprint.tscn")
 		nb_fractal = 0
